b382f59ef5fc23f0ad3d8d7ef99d632f71911417
Eugene Chung <bluewolf.chung@gmail.com>
2018-11-28 20:01:18 +0900

    add Transaction.getActions API

--- src/main/java/io/yosemite/data/remote/chain/Transaction.java
+++ src/main/java/io/yosemite/data/remote/chain/Transaction.java
@@ -1,6 +1,7 @@
 package io.yosemite.data.remote.chain;
 
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import io.yosemite.crypto.digest.Sha256;
 import io.yosemite.data.types.EosByteWriter;
 import io.yosemite.data.types.EosType;
@@ -19,7 +20,8 @@ public class Transaction extends TransactionHeader {
     private List<Action> actions;
 
     @Expose
-    private List<TransactionExtension> transaction_extensions = new ArrayList<>();
+    @SerializedName("transaction_extensions")
+    private List<TransactionExtension> transactionExtensions = new ArrayList<>();
 
     public Transaction() {
     }
@@ -28,19 +30,19 @@ public class Transaction extends TransactionHeader {
         super(other);
         this.context_free_actions = deepCopyOnlyContainer(other.context_free_actions);
         this.actions = deepCopyOnlyContainer(other.actions);
-        this.transaction_extensions = other.transaction_extensions;
+        this.transactionExtensions = other.transactionExtensions;
     }
 
     public void addAction(Action msg) {
         if (null == actions) {
-            actions = new ArrayList<>(1);
+            actions = new ArrayList<>();
         }
 
         actions.add(msg);
     }
 
     <T> List<T> deepCopyOnlyContainer(List<T> srcList) {
-        if (null == srcList) {
+        if (null == srcList || srcList.isEmpty()) {
             return null;
         }
 
@@ -56,24 +58,31 @@ public class Transaction extends TransactionHeader {
 
         writer.putCollection(context_free_actions);
         writer.putCollection(actions);
-        writer.putCollection(transaction_extensions);
+        writer.putCollection(transactionExtensions);
     }
 
     public void setStringTransactionExtension(TransactionExtensionField field, String value) {
 
-        if (StringUtils.isEmpty(value))
-            return;
+        if (StringUtils.isEmpty(value)) return;
 
         TypeName typeName = new TypeName(value);
         EosByteWriter eosByteWriter = new EosByteWriter(8);
         typeName.pack(eosByteWriter);
-        transaction_extensions.add(new TransactionExtension(field, eosByteWriter.toBytes()));
+        transactionExtensions.add(new TransactionExtension(field, eosByteWriter.toBytes()));
     }
 
     public String getId() {
         EosByteWriter eosByteWriter = new EosByteWriter(512);
         this.pack(eosByteWriter);
         return Sha256.from(eosByteWriter.toBytes()).toString();
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    public List<TransactionExtension> getTransactionExtensions() {
+        return transactionExtensions;
     }
 }
 
